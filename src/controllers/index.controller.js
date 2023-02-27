a3495a81885c79e9f7eb80bdd3387a2f6d59a253
sigleto <sigleto@gmail.com>
2023-02-27 07:45:25 +0100

    se añade URL de mongo a los controllers

--- src/controllers/index.controller.js
+++ src/controllers/index.controller.js
@@ -1,9 +1,9 @@
 
 const controllers={}
 import Task from "../models/Task.js"
+import conection from '../database.js'
 
-
-controllers.principal=(req,res)=>res.render('inicio',{titulo:"Bares que lugares"})
+controllers.principal=async(req,res)=>res.render('inicio',{titulo:"Triana-Bares"});const conect=await conection(); 
 controllers.establecimiento=(req,res)=>res.render('establecimientos',{TITULO:"GASTRO"})
 controllers.getContacto=(req,res)=>res.render('contacto',{title:"DATOS DE CONTACTO"})
 controllers.postContacto=async(req,res)=>{
--- src/database.js
+++ src/database.js
@@ -1,11 +1,12 @@
 import mongoose from "mongoose";
 mongoose.set('strictQuery', false);
 
-(async()=>{
+const conection=(async()=>{
     try{
      const URL= 'mongodb+srv://juanb:mongodbDos2.@cluster0.wdfy7nl.mongodb.net/datos-cliente?retryWrites=true&w=majority'
-    const db=await mongoose.connect(URL,{useNewUrlParser:true, useUnifiedTopology:true});
+    const db= await mongoose.connect(URL,{useNewUrlParser:true, useUnifiedTopology:true});
     console.log("nuevo dato",db.connection.name);
 }catch(error){console.error (error);} 
-})();       
+});       
   
+export default conection;
